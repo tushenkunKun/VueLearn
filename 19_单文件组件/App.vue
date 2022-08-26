3dfeeb06d2c187baebae74af3c3eb42ca88b0e2a
安丽坤 <anlk@yiche.com>
2022-08-26 18:01:47 +0800

    19_单文件组件(未通过cli运行)

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+<script>
+// 引入组件
+import School from "./School.vue";
+import Student from "./Student.vue";
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  components: { School, Student },
+};
+</script>
+<style scoped></style>
