f674ffcea0a4e999bf1ed581bf635234819ff186
安丽坤 <anlk@yiche.com>
2022-08-31 11:07:13 +0800

    19.1_VueCli-createProject-03_props属性

---
+++ 19.1_VueCli-createProject/first_vue/03_src_props属性/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <School></School>
+  </div>
+</template>
+<script>
+import School from './components/School.vue'
+export default {
+  name: "App",
+  components: {
+    School,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped></style>
