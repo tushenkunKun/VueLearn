38a323bb89b75d6d712cc98a8c02ace7860cdcb3
安丽坤 <anlk@yiche.com>
2022-09-20 18:25:56 +0800

    安装pubsub-js库,删除全局事件总线相关代码

--- 19.1_VueCli-createProject/first_vue/src/main.js
+++ 19.1_VueCli-createProject/first_vue/src/main.js
@@ -1,10 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render:(h) => h(App),
-  /* 1. 安装全局事件总线 */
-  beforeCreate(){
-    Vue.prototype.$bus = this
-  }
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
