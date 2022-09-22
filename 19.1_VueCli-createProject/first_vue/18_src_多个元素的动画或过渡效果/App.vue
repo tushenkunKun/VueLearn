c45f60ec5755dce64a8b07fb2eb7d39390f13e56
安丽坤 <anlk@yiche.com>
2022-09-22 14:50:19 +0800

    18_src_多个元素的动画或过渡效果

---
+++ 19.1_VueCli-createProject/first_vue/18_src_多个元素的动画或过渡效果/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="root">
+    <div class="app">
+      <MyList></MyList>
+    </div>
+  </div>
+</template>
+<script>
+import MyList from "./components/MyList.vue";
+
+export default {
+  name: "App",
+  components: {
+    MyList,
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
