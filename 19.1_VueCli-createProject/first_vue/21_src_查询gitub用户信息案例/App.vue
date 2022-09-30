404b65571b7ee9ddba945e94a07b1959361b9d4b
安丽坤 <anlk@yiche.com>
2022-09-30 12:01:19 +0800

    21_src_完善查询 gitub 用户信息案例

---
+++ 19.1_VueCli-createProject/first_vue/21_src_查询gitub用户信息案例/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="root">
+    <el-container>
+      <el-header height="150px">
+        <Search></Search>
+      </el-header>
+      <el-main>
+        <List></List>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+<script>
+import Search from "./components/Search.vue";
+import List from "./components/List.vue";
+export default {
+  name: "App",
+  components: {
+    Search,
+    List,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+ul,
+li {
+  list-style: none;
+}
+</style>
