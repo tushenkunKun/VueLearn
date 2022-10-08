aa6cfbcd3a585a9ba4f41dbe03ae83a13baed376
安丽坤 <anlk@yiche.com>
2022-10-08 16:57:04 +0800

    22.1_src_默认插槽

---
+++ 19.1_VueCli-createProject/first_vue/22.1_src_默认插槽/main.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import App from "./App.vue";
+
+Vue.use(ElementUI);
+
+new Vue({
+  render: (h) => h(App),
+  beforeCreate(){
+    Vue.prototype.$bus = this
+  }
+}).$mount("#app");
---
+++ 19.1_VueCli-createProject/first_vue/src/components/Category.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}分类</h3>
+    <slot>这是一个默认插槽</slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "Category",
+  props: ["title"],
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.category {
+  background-color: skyblue;
+  width: 150px;
+  height: 200px;
+}
+h3 {
+  text-align: center;
+  margin-bottom: 15px;
+  background-color: pink;
+  color: brown;
+}
+ul {
+  text-align: center;
+}
+li {
+  margin-bottom: 5px;
+}
+</style>
