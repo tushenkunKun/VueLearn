7ac9d9a893568f1789a8c954678c65b8d48a25ca
安丽坤 <anlk@yiche.com>
2022-10-11 16:52:32 +0800

    24_src_vuex中getter配置项用法

---
+++ 19.1_VueCli-createProject/first_vue/24_src_vuex中getter配置项用法/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="root">
+    <Count></Count>
+  </div>
+</template>
+<script>
+import Count from "./components/Count.vue";
+export default {
+  name: "App",
+  components: {
+    Count,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
