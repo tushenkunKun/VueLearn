a3bc1815027b77e3c3d2b6316478e3e05d1d47d5
安丽坤 <anlk@yiche.com>
2022-10-14 14:17:00 +0800

    28_src_vuex的模块化和 namepace 的用法

---
+++ 19.1_VueCli-createProject/first_vue/28_src_vuex的模块化和 namepace 的用法/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="root">
+    <Count></Count>
+    <Person></Person>
+  </div>
+</template>
+<script>
+import Count from "./components/Count.vue";
+import Person from "./components/Person.vue";
+export default {
+  name: "App",
+  components: {
+    Count,Person
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
