bb268b1870ebed3aff9e3144a98e56b9a42e7da5
安丽坤 <anlk@yiche.com>
2022-10-18 14:24:37 +0800

    29_src_路由与嵌套路由

---
+++ 19.1_VueCli-createProject/first_vue/29_src_路由与嵌套路由/pages/Apex.vue
@@ -0,0 +1,23 @@
+<template>
+ <ul class="apex">
+   <li>密客</li>
+   <li>希尔</li>
+   <li>命脉</li>
+ </ul>
+</template>
+<script>
+export default {
+  name:'Apex',
+  data(){
+    return{}
+  }
+}
+</script>
+<style scoped>
+.apex {
+  margin: 20px 0 0 30px;
+}
+li {
+  margin-bottom: 10px;
+}
+</style>
---
+++ 19.1_VueCli-createProject/first_vue/src/pages/Lol.vue
@@ -0,0 +1,23 @@
+<template>
+ <ul class="lol">
+   <li>EZ</li>
+   <li>女警</li>
+   <li>希维尔</li>
+ </ul>
+</template>
+<script>
+export default {
+  name:'Lol',
+  data(){
+    return{}
+  }
+}
+</script>
+<style scoped>
+.lol {
+  margin: 20px 0 0 30px;
+}
+li {
+  margin-bottom: 10px;
+}
+</style>
