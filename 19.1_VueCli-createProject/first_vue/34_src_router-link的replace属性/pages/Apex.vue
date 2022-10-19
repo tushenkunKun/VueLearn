4fec22994e5e9ce51d42d4180026cb4ef16dcdd3
安丽坤 <anlk@yiche.com>
2022-10-19 14:50:36 +0800

    34_src_router-link的replace属性

---
+++ 19.1_VueCli-createProject/first_vue/34_src_router-link的replace属性/pages/Apex.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul class="apex">
+    <li v-for="person in personInfo" :key="person.id">
+      <!-- 第一种写法, 模版字符串 -->
+      <!-- <router-link :to="`/home/apex/detail/${person.id}/${person.name}`">{{ person.name }}</router-link> -->
+      <!-- 第二种写法, 对象写法: 这种写法里params 必须和name 配合使用, 不能使用 path -->
+      <router-link
+        :to="{
+          name: 'apexgame',
+          params: {
+            id: person.id,
+            name: person.name,
+          },
+        }"
+      >
+        {{ person.name }}
+      </router-link>
+    </li>
+    <router-view></router-view>
+  </ul>
+</template>
+<script>
+import { nanoid } from "nanoid";
+export default {
+  name: "Apex",
+  data() {
+    return {
+      personInfo: [
+        { id: nanoid(), name: "密客" },
+        { id: nanoid(), name: "希尔" },
+        { id: nanoid(), name: "命脉" },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.apex {
+  margin: 20px 0 0 30px;
+}
+li {
+  margin-bottom: 10px;
+}
+li a {
+  text-decoration: none;
+  color: olivedrab;
+}
+</style>
