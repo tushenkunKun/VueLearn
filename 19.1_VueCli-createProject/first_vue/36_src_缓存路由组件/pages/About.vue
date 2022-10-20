87787776758c4cf5a00bea4b1aa5e0b07b091252
安丽坤 <anlk@yiche.com>
2022-10-20 13:45:05 +0800

    36_src_缓存路由组件

---
+++ 19.1_VueCli-createProject/first_vue/36_src_缓存路由组件/pages/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>这是 About 组件</h1>
+    <p>
+      你好可樱, 恭喜你又长大一岁, 这是我成为双尾猫以来陪你过的第一个生日. 不得不说哥们儿就一臭直男, 是既没有米又没有才艺, 所以就在这里简简单单 的祝你生日快乐, 希望你身体健健康康, 直播事业蒸蒸日上,
+      能和双尾猫们一起度过更多的时光, 加油
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "About",
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped></style>
