67064c013402cfe5dc93062ecbe1caaee76fd274
安丽坤 <anlk@yiche.com>
2022-10-28 18:14:22 +0800

    20.4_src_watch(初始)

--- 20_vue3learn/src/components/Demo.vue
+++ 20_vue3learn/src/components/Demo.vue
@@ -1,41 +1,14 @@
 <template>
-  <div>
-    姓:
-    <input type="text" v-model="lastName" />
-  </div>
-  <div>
-    名:
-    <input type="text" v-model="firstName" />
-  </div>
-  <div>
-    全名:
-    <input type="text" v-model="fullName" />
-  </div>
+  <h3>{{ num }}</h3>
 </template>
 
 <script>
-import { reactive, ref, computed } from "vue";
+import { reactive, ref } from "vue";
 export default {
   name: "Demo",
   setup(props, context) {
-    let firstName = ref("");
-    let lastName = ref("");
-    /* computed 的简写形式 */
-    /* let fullName = computed(() => {
-      return lastName.value + "-" + firstName.value;
-    }); */
-    /* computed 的完全形式 */
-    let fullName = computed({
-      get: () => {
-        return lastName.value + "-" + firstName.value;
-      },
-      set:(value)=>{
-        let nameArr = value.split('-');
-        lastName.value = nameArr[0];
-        firstName.value = nameArr[1];
-      }
-    });
-    return { firstName, lastName, fullName };
+    let num = ref(0);
+    return { num };
   },
 };
 </script>
