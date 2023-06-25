858a204298c3c590b143e7f683c73bbda83b1425
安丽坤 <anlk@yiche.com>
2023-06-25 13:32:49 +0800

    ref 相关

--- 20_vue3learn/src/components/Demo.vue
+++ 20_vue3learn/src/components/Demo.vue
@@ -8,6 +8,10 @@ export default {
   name: "Demo",
   setup(props, context) {
     let num = ref(0);
+    /* 1. 监视 ref 定义的一个响应式数据 */
+    /* 2. 监视 ref 定义的多个响应式数据 */
+    /* 3. 监视 reactive 定义的一个响应式数据(无法正确获取到 oldValue 的值) */
+    /* 3. 监视 reactive 定义的一个响应式数据(无法正确获取到 oldValue 的值) */
     return { num };
   },
 };
